7323f31482d7ee9fe25e298950bf5432b7770cc4
Andy D <mindovermiles262@gmail>
2020-08-30 17:15:17 -0500

    save and load decks from disk

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // We will create a "deck" type to hold the information for a deck of cards
 // Create a new type ('deck'), which is a slice of string
@@ -8,12 +12,13 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
+	// Creates a new "deck" of cards
 	cards := deck{}
 	cardSuits := []string{
 		"Hearts",
 		"Diamonds",
-		// "Spades",
-		// "Clubs",
+		"Spades",
+		"Clubs",
 	}
 	cardValues := []string{
 		"Ace",
@@ -41,6 +46,7 @@ func newDeck() deck {
 }
 
 func (d deck) print() {
+	// Prints contents of deck, each card on its own line
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
@@ -48,4 +54,28 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	// Converts deck object into CSV string
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) save(fileName string) error {
+	// Writes deck to CSV file
+	msg := []byte(d.toString())
+	err := ioutil.WriteFile(fileName, msg, 0644)
+	return err
+}
+
+func openFromFile(filename string) deck {
+	byteslice, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Error handling, return new deck if there is an error
+		fmt.Println("[!]", err)
+		return newDeck()
+	}
+	strDeck := string([]byte(byteslice))
+	strSlice := strings.Split(strDeck, ",")
+	return deck(strSlice)
 }
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 func main() {
 	cards := newDeck()
 	// cards.print()
-	hand, remainingDeck := deal(cards, 5)
+	// hand, remainingDeck := deal(cards, 5)
 
-	hand.print()
-	remainingDeck.print()
+	// hand.print()
+	// remainingDeck.print()
+
+	// fmt.Println(cards.toString())
+	cards.save("deck.csv")
+
+	restoredDeck := openFromFile("deck.csv")
+	restoredDeck.print()
 }

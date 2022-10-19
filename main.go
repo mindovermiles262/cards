2ddf9af08b4991d4d3bf693c376109936f5463a9
Andrew Duss <andy.duss@storable.com>
2022-10-19 15:52:47 -0600

    pointers
    Signed-off-by: Andrew Duss <andy.duss@storable.com>

--- main.go
+++ main.go
@@ -1,18 +1,58 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	age       int
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode string
+}
+
 func main() {
-	cards := newDeck()
-	// cards.print()
-	// hand, remainingDeck := deal(cards, 5)
+	// john := person{"John", "Steinbeck", 66}
 
-	// hand.print()
-	// remainingDeck.print()
+	// john := person{
+	// 	firstName: "John",
+	// 	lastName:  "Steinbeck",
+	// 	age:       66,
+	// }
 
-	// fmt.Println(cards.toString())
-	// cards.save("deck.csv")
+	var john person
+	john.firstName = "John"
+	john.lastName = "Steinbeck"
+	john.age = 66
+	john.contactInfo = contactInfo{
+		email:   "john@gmail.com",
+		zipCode: "90210",
+	}
 
-	// restoredDeck := openFromFile("deck.csv")
-	// restoredDeck.print()
-	cards.shuffle()
-	cards.print()
+	// fmt.Println(john.contact.email)
+	// fmt.Printf("%+v\n", john)
+	// john.print()
+
+	// john.updateName("Jonathan")
+	// john.print() //=> "John"
+
+	// johnPointer := &john
+	// johnPointer.updateName("Jonathan") // Updates the memory address for "John"
+	// john.print()                       //=> "Jonathan"
+
+	// Shortcut to deal with pointers. Golang allows us to call a function that
+	// requires a pointer by either the pointer or instance.
+	john.updateName("Jonathan")
+	john.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
